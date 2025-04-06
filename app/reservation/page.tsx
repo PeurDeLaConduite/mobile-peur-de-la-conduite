d87565ba0b3b6ad55efaea85121f9ccf58e74293
Jerem16 <jeremy.lemaignent@gmail.com>
2025-04-06 14:44:42 +0200

    init 22

--- app/reservation/page.tsx
+++ app/reservation/page.tsx
@@ -1,3 +1,5 @@
+import Space from "../../src/utils/Space";
+import Loader from "../../src/components/loader/Loader";
 import { Metadata } from "next";
 export const metadata: Metadata = {
     title: "Réservation",
@@ -6,17 +8,14 @@ export const metadata: Metadata = {
 export default async function Page() {
     // await new Promise((r) => setTimeout(r, 1000));
     return (
-        <section className="section" id="s1">
+        <section className="section" id="hs">
             <div className="fixed-menu"></div>
             <h2>Réservation</h2>
-            <div className="s1">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Velit aliquid provident magnam, delectus nobis est sunt iste
-                    aut at error assumenda voluptas, harum sed consequuntur
-                    voluptate soluta deleniti voluptatum consequatur.
-                </p>
-            </div>
+            <p>
+                Cette section n’est pas encore disponible. <Space />
+                Merci de votre patience
+            </p>
+            <Loader />
         </section>
     );
 }
