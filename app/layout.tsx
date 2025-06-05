57610c4282438b626957f2c705a1e40ca2ecfe2a
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-05 09:36:10 +0200

    feature Base One 321

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,7 +92,7 @@ export default function RootLayout({
                     href="./deferCss.css"
                     fetchPriority="low"
                 />
-                {/* <link rel="preload" href="./tarifs.css" as="style" />
+                <link rel="preload" href="./tarifs.css" as="style" />
                 <link
                     rel="stylesheet"
                     href="./tarifs.css"
@@ -103,7 +103,7 @@ export default function RootLayout({
                     rel="stylesheet"
                     href="./blog.css"
                     fetchPriority="low"
-                /> */}
+                />
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
