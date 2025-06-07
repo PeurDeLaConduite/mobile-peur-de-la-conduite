bd7438fc67bed10d26afcd281cbe39cf9d603957
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-07 15:35:54 +0200

    feature Base One

--- src/components/Blog/ButtonPage.tsx
+++ src/components/Blog/ButtonPage.tsx
@@ -1,7 +1,5 @@
 // src/components/button/ButtonPage.tsx
 import React from "react";
-import { BackButton } from "@components/button/Buttons";
-// import Link from "next/link";
 
 interface ButtonPageProps {
     href: string;
@@ -11,7 +9,19 @@ interface ButtonPageProps {
 const ButtonPage: React.FC<ButtonPageProps> = ({ href, className = "" }) => (
     <div className={`btn-P ${className}`.trim()}>
         <div className="center-fixed">
-            <BackButton href={href} label={""} className="btn-P__back" />
+            <a className="btn-P__back" title="Retour" href={href}>
+                <svg
+                    focusable="false"
+                    fill="#fff"
+                    aria-hidden="true"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    data-testid="ArrowBackIcon"
+                >
+                    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"></path>
+                </svg>
+            </a>
         </div>
     </div>
 );
