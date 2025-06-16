77dc1cd00273cee1d470aa0e15c403f0c64cc100
Jerem16 <jeremy.lemaignent@gmail.com>
2025-06-16 12:25:54 +0200

    feature Base One

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -15,11 +15,18 @@ export const metadata: Metadata = {
                 "https://desktop.peur-de-la-conduite.fr/blog",
         },
     },
+    description:
+        "Découvrez une série d’articles dédiés à la peur de la conduite : conseils pour surmonter l’amaxophobie, gérer le stress au volant, réussir le permis et retrouver confiance.",
 };
 
 export default async function Page() {
     return (
         <PageContainer id="blog" title="Blog" icon={<BlogIcon />}>
+            {/* <p className="btn-SB flx-c">
+                Retrouvez ici nos articles sur l’amaxophobie, les techniques de
+                gestion du stress et le coaching pour réussir votre permis.
+            </p> */}
+
             <BlogClientWrapper />
         </PageContainer>
     );
