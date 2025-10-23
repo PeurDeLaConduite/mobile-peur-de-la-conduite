d0afd6a635c6c619a7bf09735b76c1bfed3cb3d2
Jerem16 <jeremy.lemaignent@gmail.com>
2025-10-23 21:55:42 +0200

    start

--- app/DesktopRedirect.tsx
+++ app/DesktopRedirect.tsx
@@ -1,31 +1,20 @@
-// "use client";
+"use client";
 
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// export default function DesktopRedirect() {
-//     useEffect(() => {
-//         if (typeof window === "undefined") return;
+export default function DesktopRedirect() {
+    useEffect(() => {
+        if (typeof window === "undefined") return; // sécurité côté SSR
 
-//         const isDesktop = window.innerWidth >= 900;
-//         if (!isDesktop) return;
+        if (window.innerWidth < 900) {
+            return;
+        }
+        window.location.href =
+            "https://desktop.peur-de-la-conduite.fr" +
+            window.location.pathname +
+            window.location.search +
+            window.location.hash;
+    }, []);
 
-//         // Définir le cookie deviceType=desktop (visible côté serveur)
-//         document.cookie = [
-//             "deviceType=desktop",
-//             "Path=/",
-//             "Domain=.peur-de-la-conduite.fr",
-//             "Secure",
-//             "SameSite=Strict",
-//             "Max-Age=" + 60 * 60 * 24 * 30, // 30 jours
-//         ].join("; ");
-
-//         // Redirection immédiate vers la version desktop
-//         window.location.href =
-//             "https://peur-de-la-conduite.fr" +
-//             window.location.pathname +
-//             window.location.search +
-//             window.location.hash;
-//     }, []);
-
-//     return null;
-// }
+    return null;
+}
