a8d8ef93c4cb8d77a6ba109f550d0624ba7679cd
Jerem16 <jeremy.lemaignent@gmail.com>
2025-10-23 18:06:15 +0200

    start

--- app/DesktopRedirect.tsx
+++ app/DesktopRedirect.tsx
@@ -7,10 +7,10 @@ export default function DesktopRedirect() {
         if (typeof window === "undefined") return; // sécurité côté SSR
 
         if (window.innerWidth < 900) {
-            return; 
-        }    
+            return;
+        }
         window.location.href =
-            "https://peur-de-la-conduite.fr" +
+            "https://desktop.peur-de-la-conduite.fr" +
             window.location.pathname +
             window.location.search +
             window.location.hash;
